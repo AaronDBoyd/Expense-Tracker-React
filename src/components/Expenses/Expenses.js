efdfc88a366b3a09af3bc240443e474950b89933
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-16 15:34:21 -0700

    pass data up through function props

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './Expenses.css'
