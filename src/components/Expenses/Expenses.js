3b758e7afdc815b1a481c2e7d3c50950c796f840
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-16 19:10:58 -0700

    move conditional outside of return

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,35 +2,42 @@ import React, { useState } from "react";
 // import PropTypes from "prop-types";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
-import ExpensesFilter from './ExpensesFilter'
-import './Expenses.css'
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const [filteredYear, setFilteredYear] = useState('2020');
-
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
-  const byYear = props.items.filter( item => item.date.getFullYear().toString() === filteredYear);
+  const byYear = props.items.filter(
+    (item) => item.date.getFullYear().toString() === filteredYear
+  );
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (byYear.length > 0) {
+    expensesContent = byYear.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
 
   return (
     <>
-    <Card className="expenses">
-    <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-    {byYear.length === 0 && <p> No expenses found. </p>}
-    {byYear.length > 0 &&
-    byYear.map(expense => (
-      <ExpenseItem
-      key={expense.id}
-      title={expense.title}
-      amount={expense.amount}
-      date={expense.date}
-    />
-      ))}
-      
-    </Card>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expensesContent}
+      </Card>
     </>
   );
 }
